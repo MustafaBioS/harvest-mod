c86f7b7e2f429c040d14b0929f8e3fbcec426408
Nullskulls <nullskulls@proton.me>
2026-09-19 12:32:38 +0300

    Add seed tracking and storage logic to HarvesterBlockEntity

--- src/main/java/com/harvestmod/block/entity/HarvesterBlockEntity.java
+++ src/main/java/com/harvestmod/block/entity/HarvesterBlockEntity.java
@@ -5,23 +5,50 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.CropBlock;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 public class HarvesterBlockEntity extends BlockEntity {
-    private int tick_count = 0;
+    private int tickCount = 0;
+    private int seedsHeld = 0;
 
-    private static final int LOOKUP_RANGE = 24;
-    private static final int LOOKUP_Y_DEVIATION = 10;
+    private static final int LOOKUP_RANGE = 12;
+    private static final int MAX_SEEDS_HELD = 256;
 
     public HarvesterBlockEntity(BlockPos pos, BlockState state) {
         super(ModBlockEntities.HARVESTER, pos, state);
     }
 
-    public static void tick(World world, BlockPos pos, BlockState state, HarvesterBlockEntity be) {
-        be.tick_count++;
-        if (be.tick_count >= 100){
-            be.tick_count = 0;
+    @Override
+    protected void writeNbt(NbtCompound nbt, RegistryWrapper.WrapperLookup registries) {
+        super.writeNbt(nbt, registries);
+        nbt.putInt("SeedsHeld", seedsHeld);
+    }
+
+    @Override
+    protected void readNbt(NbtCompound nbt, RegistryWrapper.WrapperLookup registries) {
+        super.readNbt(nbt, registries);
+        seedsHeld = nbt.getInt("SeedsHeld");
+    }
+
+    public int addSeeds(int seedCount){
+        int deltaSeeds = Math.clamp(seedsHeld+seedCount, 0, MAX_SEEDS_HELD) - seedsHeld;
+        seedsHeld += deltaSeeds;
+        markDirty();
+        return seedCount-deltaSeeds;
+
+    }
+
+    public int getSeedsHeld(){
+        return seedsHeld;
+    }
+
+    public void tick(World world, BlockPos pos, BlockState state, HarvesterBlockEntity be) {
+        be.tickCount++;
+        if (be.tickCount >= 100){
+            be.tickCount = 0;
             HarvestMod.LOGGER.info("Harvester ticking at {} (is_client: {})", pos, world.isClient);
         }
 
@@ -36,8 +63,12 @@ public class HarvesterBlockEntity extends BlockEntity {
             BlockState cropState = world.getBlockState(cropPos);
 
             if (cropState.getBlock() instanceof CropBlock crop && crop.isMature(cropState)) {
+                world.removeBlock(cropPos, false);
                 Block.dropStacks(cropState, world, cropPos);
-                world.setBlockState(cropPos, crop.withAge(0), Block.NOTIFY_LISTENERS);
+                if (seedsHeld > 0) {
+                    world.setBlockState(cropPos, crop.withAge(0), Block.NOTIFY_LISTENERS);
+                    seedsHeld--;
+                }
                 HarvestMod.LOGGER.info("Harvested crop at {}", cropPos);
             }
         }
